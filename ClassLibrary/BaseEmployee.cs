ba9c87e1732b3ab9f2944be429c440e4cc2051e1
Antonis-Ioannou <antonios.ioannou@protonmail.com>
2021-10-18 18:22:51 +0300

    Add project files.

---
+++ ClassLibrary/BaseEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public abstract class BaseEmployee : IEmployee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+       
+        public decimal Salary { get; set; }
+
+        public virtual void CalculatePerHourRate(int rank)
+        {
+            decimal baseAmount = 10M;
+
+            Salary = baseAmount + (rank * 2);
+        }
+    }
+}
---
+++ Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager acountingVP = new Manager();
+
+            acountingVP.FirstName = "Bruce";
+            acountingVP.LastName = "Banner";
+            acountingVP.CalculatePerHourRate(4);
+
+            Employee emp = new Manager();
+
+            emp.FirstName = "Jack";
+            emp.LastName = "Kirby";
+            emp.AssignManager(acountingVP);
+            emp.CalculatePerHourRate(2);
+
+            Console.WriteLine($"{emp.FirstName}'s salary is ${emp.Salary}/hour. {emp.Manager.FirstName}");
+
+            Console.ReadLine();
+        }
+    }
+}
+ 
